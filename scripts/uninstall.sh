94a2d4967d0d781623d02efd8b95ea0a4fd01b7f
richard-dp <6692307+richard-dp@users.noreply.github.com>
2025-08-09 18:18:29 +0200

    Added install/uninstall scripts and updated README.md

---
+++ scripts/uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+systemctl --user disable nova-chatmix --now
+rm ~/.config/systemd/user/nova-chatmix.service
+systemctl --user daemon-reload
+
+rm ~/.local/bin/nova.py 
+
+sudo rm /etc/udev/rules.d/50-nova-pro.rules
+
+sudo udevadm control --reload-rules
+sudo udevadm trigger
