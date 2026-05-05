fa15b5c510c65c267ac040a323354b36998e0183
richard-dp <6692307+richard-dp@users.noreply.github.com>
2026-05-05 21:01:03 +0200

    Updated sink name for arctis-7p

--- nova-chatmix.py
+++ nova-chatmix.py
@@ -184,7 +184,7 @@ class NovaProWireless:
 
     # PipeWire Names
     ## String used to automatically select output sink
-    PW_OUTPUT_SINK_AUTODETECT = "SteelSeries_Arctis_Nova_Pro"
+    PW_OUTPUT_SINK_AUTODETECT = "SteelSeries_Arctis_7"
     ## Names of virtual sound devices
     PW_GAME_SINK = "NovaGame"
     PW_CHAT_SINK = "NovaChat"
